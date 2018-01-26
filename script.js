b08bf7de7f7fca804e971f8fdf1e297fccd9d37f
Imre Bartis <imre.bartis@siili.com>
2018-01-26 08:45:04 +0200

    binding to CSS properties

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 var growler = new Vue({
   el: "#growler",
   data: {
-    appName: '<a href="/">Growler</a>', // binding to html
-    "appLogo": 'logo.png'
+    appName: '<a href="/">Growler</a>',
+    appStyle: {
+      fontFamily: 'Helvetica',
+      backgroundColor: '#BADA55',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    accentColor: {
+      color: '#FF6A00'
+    },
+    paragraphs: {
+      fontFamily: 'Verdana',
+      margin: 0
+    }
   }
 });
