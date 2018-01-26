000af85289a33bb82299c9f5942e84bb593afd1b
Imre Bartis <imre.bartis@siili.com>
2018-01-26 09:18:13 +0200

    binding to CSS classes

--- script.js
+++ script.js
@@ -2,19 +2,8 @@ var growler = new Vue({
   el: "#growler",
   data: {
     appName: '<a href="/">Growler</a>',
-    appStyle: {
-      fontFamily: 'Helvetica',
-      backgroundColor: '#BADA55',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    accentColor: {
-      color: '#FF6A00'
-    },
-    paragraphs: {
-      fontFamily: 'Verdana',
-      margin: 0
-    }
+    // binding to CSS classes
+    accentColor: 'accent-color',
+    intro: 'intro'
   }
 });
