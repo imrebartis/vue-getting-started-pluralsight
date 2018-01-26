86b1ae5d2b892031c1b553f0b457c4bf7eea2f8b
Imre Bartis <imre.bartis@siili.com>
2018-01-26 12:00:34 +0200

    input field types

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var growler = new Vue({
-  el: '#growler',
+var app = new Vue({
+  el: '#app',
   data: {
-      appName: 'Growler',
-      emailMessage: ''
+      colorInput: '(nothing)',
+      dateInput: '(nothing)',
+      dateTimeLocalInput: '(nothing)',
+      emailInput: '(nothing)',
+      monthInput: '(nothing)',
+      numberInput: '(nothing)',
+      passwordInput: '(nothing)',
+      rangeInput: 0,
+      telInput: '(nothing)',
+      textInput: '(nothing)',
+      timeInput: '(nothing)',
+      searchInput: '(nothing)',
+      urlInput: '(nothing)',
+      weekInput: '(nothing)'
   }
-});     
+});        
