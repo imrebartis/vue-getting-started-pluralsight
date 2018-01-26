264fd4beb0b54a5ca6831a518ad1391d2451e79d
Imre Bartis <imre.bartis@siili.com>
2018-01-26 11:13:31 +0200

    binding an input field with a text type

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 var growler = new Vue({
-  el: "#growler",
+  el: '#growler',
   data: {
-    appName: '<a href="/">Growler</a>',
-    // binding to CSS classes
-    accentColor: 'accent-color',
-    intro: 'intro'
+      appName: 'Growler',
+      query: ''
   }
-});
+});     
