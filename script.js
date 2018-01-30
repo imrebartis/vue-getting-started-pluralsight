b20700df8c2b393c8a1bb071ccdb4cb771679aa6
Imre Bartis <imre.bartis@siili.com>
2018-01-30 11:07:36 +0200

    using the once modifier propagation 2 (button disabled after one click)

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ var growler = new Vue({
     el: '#growler',
     data: {
         query: '',
+        isRunning: false,
         history: ''
     },
     methods: {
         executeSearch: function() {
+            this.isRunning = true;
             this.history += 'Search button clicked. Query "' + this.query + '" was entered.\n';
+            //document.forms[0].submit();                        
         },
         buttonParentClick: function() {
             this.history += 'Search button parent clicked.\n';
         },
         buttonGrandparentClick: function() {
-            this.history += 'Search button grandparent clicked.\n\n';
+            this.history += 'Search button grandparent clicked.\n';
         }
     }
 });      
