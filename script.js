27f9b57ff1967b8ad16f86a0d3e5d312924dd5b6
Imre Bartis <imre.bartis@siili.com>
2018-01-30 11:01:21 +0200

    using the once modifier propagation

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 var growler = new Vue({
-  el: '#growler',
-  data: {
-      query: ''
-  },
-  methods: {
-      executeSearch: function(v, e) {
-          var s = JSON.stringify(v);
-          var msg = 'Value: ' + s + '\n' +
-              'Query: ' + this.query + '\n' +
-              'Button: ' + e.target.innerText
-          ;
-          alert(msg);
-      }                    
-  }                
-});    
+    el: '#growler',
+    data: {
+        query: '',
+        history: ''
+    },
+    methods: {
+        executeSearch: function() {
+            this.history += 'Search button clicked. Query "' + this.query + '" was entered.\n';
+        },
+        buttonParentClick: function() {
+            this.history += 'Search button parent clicked.\n';
+        },
+        buttonGrandparentClick: function() {
+            this.history += 'Search button grandparent clicked.\n\n';
+        }
+    }
+});      
