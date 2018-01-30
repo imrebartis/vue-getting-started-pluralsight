4a7c57f4af66c8f408152eb83ffeb570f48973b0
Imre Bartis <imre.bartis@siili.com>
2018-01-30 12:26:11 +0200

    reacting to the enter key

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-Vue.config.keyCodes = {
-    sb: 32
-};
+function checkForEnter(e) {
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 13) {
+        growler.executeSearch();
+        return false;
+    }
+    return true;
+}
 var growler = new Vue({
     el: '#growler',
     data: {
@@ -8,9 +13,14 @@ var growler = new Vue({
         history: ''
     },
     methods: {
-        openInfo: function(e) {
-            e.preventDefault();
-            this.history += 'Show app information.\n';
+        evaluateKey: function(e) {
+            var code = (e.keyCode ? e.keyCode : e.which);                        
+            this.history += 'key clicked: "' + e.key + '" (' + code + ')\n';
+        },
+        executeSearch: function(e) {
+            if (this.query) {
+                this.history += 'Search button clicked. Query "' + this.query + '" was entered.\n';
+            }
         },
         clearHistory: function() {
             this.history = '';
