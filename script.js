189cb0c921be6ad1e2d75b970019ea7beb7b98e8
Imre Bartis <imre.bartis@siili.com>
2018-01-30 13:02:55 +0200

    mouse button events

--- script.js
+++ script.js
@@ -2,21 +2,41 @@ var growler = new Vue({
     el: '#growler',
     data: {
         query: '',
-        history: ''
+        history: '',
+        showContextMenu: false,
+        top: '0px',
+        left: '0px'
     },
     methods: {
-        evaluateKey: function(e) {
-            var code = (e.keyCode ? e.keyCode : e.which);                        
-            this.history += 'key clicked: "' + e.key + '" (' + code + ')\n';
-        },
-        executeSearch: function(e) {
-            if (this.query) {
-                this.history += 'Search button clicked. Query "' + this.query + '" was entered.\n';
-                //document.forms[0].submit();
-            }
+        onBlockClick: function(e) {
+            this.history += 'Mouse was pressed down!\n';
+            var button = 'Unknown';
+            if (e.button === 0) {
+                button = 'Left';
+            } else if (e.button === 1) {
+                button = 'Middle';
+            } else if (e.button === 2) {
+                button = 'Right';
+                this.showContextMenu = true;
+                this.top = e.y + 'px';
+                this.left = e.x + 'px';
+                console.log(e);
+            }          
+            this.history += button + ' button was pressed down.\n---\n\n';
         },
         clearHistory: function() {
             this.history = '';
+        },
+        closeContextMenu: function() {
+            this.showContextMenu = false;                        
+        },
+        onCopyClick: function() {
+            alert('copy something');
+            this.closeContextMenu();
+        },
+        onPasteClick: function() {
+            alert('paste something');
+            this.closeContextMenu();                        
         }
     }
 });        
