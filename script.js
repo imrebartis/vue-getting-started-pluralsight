25f00859258aa5b01ee2554a283aadb752060614
Imre Bartis <imre.bartis@siili.com>
2018-02-09 08:52:01 +0200

    v-if, v-else-if, v-else

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-setTimeout(initializeGrowler, 3000);
-            function initializeGrowler() {
-                var growler = new Vue({
-                    el: '#growler',
-                    data: {
-                        history: ''
-                    },
-                    created: function() {
-                        this.history = 'Growler created!';
-                    }
-                });        
+var growler = new Vue({
+    el: '#growler',
+    data: {
+        beers: []
+    },
+    methods: {
+        executeSearch: function() {
+            var randomCount = Math.floor(Math.random() * 4);
+            this.beers = [];
+            for (var i=0; i<randomCount; i++) {
+                var beer = {
+                    id: (i+1),
+                    name: 'Beer #' + (i+1)
+                };
+                this.beers.push(beer);
             }
+        }
+    }
+});    
