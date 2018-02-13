422edb47c274dc7837a7a6f53ebd7e6883f5f1bd
Imre Bartis <imre.bartis@siili.com>
2018-02-13 08:56:56 +0200

    iterating thru an array of objects (v-for)

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 var growler = new Vue({
     el: '#growler',
     data: {
-        currentUser: {
-            firstName: 'Chad',
-            lastName: 'Campbell',
-            fingers: 10,
-            socialMedia: {
-                twitter: '@ecofic',
-                youtube: 'https://www.youtube.com/c/ecofic'
-            },
-            tags: [ 'male', 'scorpio' ]
-        }
+        pages: [
+            { number: 1, url: '?page=1' },
+            { number: 2, url: '?page=2' },
+            { number: 3, url: '?page=3' },
+        ]
     }
-});        
+});       
