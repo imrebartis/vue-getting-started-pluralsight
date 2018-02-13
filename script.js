1e8d41d4b171e762fe1f70e56b0fe01f05fc4b5d
Imre Bartis <imre.bartis@siili.com>
2018-02-13 11:13:09 +0200

    removing items from an array with splice

--- script.js
+++ script.js
@@ -7,18 +7,14 @@ var growler = new Vue({
             'Ahool Ale',
             'Tikibalang Barley Wine'
         ],
-        newBeerName: '',
-        count: 4// This property exists to test the fact that the push function returns the new length
+        newBeerName: ''
     },
     methods: {
-        unshiftClick: function() {
-            this.count = this.beers.unshift(this.newBeerName);
-            this.newBeerName = '';
-        },
-        shiftClick: function() {
-            var shifted = this.beers.shift();
-            console.log('Shifted:');
-            console.log(shifted);
+        spliceClick: function() {
+            // removing the 3 beers starting at index 1
+            // i.e. everything except Pope Lick Porter
+            var removed = this.beers.splice(1, 3);
+            console.log(removed);
         }
     }                
 });     
