98767c451d1e78b01376c26d2942947e3ea05dd2
Imre Bartis <imre.bartis@siili.com>
2018-02-13 11:39:49 +0200

    updating an array element with Vue.set or splice

--- script.js
+++ script.js
@@ -7,24 +7,13 @@ var growler = new Vue({
             'Ahool Ale',
             'Tikibalang Barley Wine'
         ],
-        removed: []
+        newBeerName: ''
     },
     methods: {
-        removeClick: function() {
-            this.removed = this.beers.splice(1, 2);
-        },
-        // adding back the items removed with removeClick:
-        replaceClick: function() {
-            try {                            
-                for (var i=0; i<this.removed.length; i++) {
-                    // we're adding beers after the first item of the array; 
-                    // since we don't want to remove items, the second param is 0;
-                    // this.removed[i] adds the name of the item
-                    this.beers.splice(1, 0, this.removed[i]);
-                }
-            } catch (ex) {
-                console.log(ex);
-            }
+        updateClick: function() {
+            Vue.set(this.beers, 2, 'Ahool Pale Ale');
+            // splice could be used here, too:
+            // this.beers.splice(2, 1, 'bla')
         }
     }                
-});
+});    
