7eacdee2030dbb89defc8c9ea90e977273c87bc2
Imre Bartis <imre.bartis@siili.com>
2018-02-13 08:01:51 +0200

    looping a specific number of times

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 var growler = new Vue({
     el: '#growler',
     data: {
-        beers: []
+        pageCount: 12
     },
     methods: {
-        executeSearch: function() {
-            var randomCount = Math.floor(Math.random() * 4);
-            this.beers = [];
-            for (var i=0; i<randomCount; i++) {
-                var beer = {
-                    id: (i+1),
-                    name: 'Beer #' + (i+1)
-                };
-                this.beers.push(beer);
-            }
+        search: function(pageNumber) {
+            alert('Loading page number: ' + pageNumber);
         }
     }
 });        
