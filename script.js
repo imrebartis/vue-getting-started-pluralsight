715b4d773b4168f1931e16bdfe1af3ad89e09b77
Imre Bartis <imre.bartis@siili.com>
2018-02-13 09:47:29 +0200

    sort function with strings

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ var growler = new Vue({
     el: '#growler',
     data: {
         beers: [
-            { name: 'Tikibalang Barley Wine', abv:9.6 },
-            { name: 'Hyote Chocolate Stout', abv:7.4 },
-            { name: 'Pope Lick Porter', abv:6.5 },
-            { name: 'Ahool Ale', abv:5.4 },
-            { name: 'North Adjule Lager', abv:3.7 }                                                
-        ],
-        maxAbv:7.0
-    }
-});  
+            'Tikibalang Barley Wine',
+            'Hyote Chocolate Stout',
+            'Pope Lick Porter',
+            'Ahool Ale',
+            'North Adjule Lager'
+        ]
+    },
+    methods: {
+        sortClick: function() {
+            this.beers.sort();
+        }
+    }                
+});   
