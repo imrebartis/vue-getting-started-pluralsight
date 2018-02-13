364c92b965d46513b03f38b4fbd358d98239d5bf
Imre Bartis <imre.bartis@siili.com>
2018-02-13 10:33:53 +0200

    using the pop function

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ var growler = new Vue({
         beers: [
             'Ahool Ale',
             'Hyote Chocolate Stout',
-            'Tikibalang Barley Wine'
+            'Tikibalang Barley Wine',
+            'Pope Lick Porter'
         ],
         newBeerName: '',
-        count: 3// This property exists to test the fact that the push function returns the new length
+        count: 4// This property exists to test the fact that the push function returns the new length
     },
     methods: {
         pushClick: function() {
+            if (this.newBeerName != '') {
             this.count = this.beers.push(this.newBeerName);
             this.newBeerName = '';
             console.log(this.count)
+            }
+        },
+        popClick: function() {
+            var popped = this.beers.pop();
+            console.log('Popped:');
+            console.log(popped);
         }
     }                
-});    
+});        
