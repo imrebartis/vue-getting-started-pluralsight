026a6ea9e0acd6b0e2e048d583078cf9d0d1a5fb
Imre Bartis <imre.bartis@siili.com>
2018-02-14 14:41:27 +0200

    example of a watched property

--- script.js
+++ script.js
@@ -2,18 +2,46 @@ var growler = new Vue({
     el: '#growler',
     data: {
         beers: [
-            'Pope Lick Porter',
-            'Hyote Chocolate Stout',
-            'Ahool Ale',
-            'Tikibalang Barley Wine'
+            { name: 'Ahool Ale', price: 2.80 },
+            { name: 'Agogwe Ale', price: 2.38 },
+            { name: 'Aswang Ale', price: 3.05 },
+            { name: "Buru's Barley Wine", price: 2.95 },
+            { name: 'Hyote Chocolate Stout', price: 4.68 },
+            { name: 'Igopogo Pilsner', price: 3.40 },
+            { name: 'Jackalobe Lager', price: 2.49 },
+            { name: 'Mahamba Barley Wine', price: 4.89 },
+            { name: 'Megalodon Pale Ale', price: 3.76 },
+            { name: 'Pope Lick Porter', price: 3.52 },
+            { name: 'Chocolate Pukwudgie Stout', price: 4.17 },
+            { name: 'Sharlie Pilsner', price: 2.92 },
+            { name: 'Sigbin Stout', price: 2.49 },
+            { name: 'Snallygaster Pale Ale', price: 3.64 },
+            { name: 'Tikibalang Barley Wine', price: 4.21 },
+            { name: 'Pale Popobawa Ale', price: 4.34 },
+            { name: 'North Adjule Lager', price: 3.84 }
         ],
-        newBeerName: ''
+        shoppingCart: [],
+        subTotal: 0.00
+    },
+    watch: {
+        shoppingCart: function() {
+            this.updateSubTotal();
+        }
+    },
+    created: function() {
+        this.updateSubTotal();
     },
     methods: {
-        updateClick: function() {
-            Vue.set(this.beers, 2, 'Ahool Pale Ale');
-            // splice could be used here, too:
-            // this.beers.splice(2, 1, 'bla')
+        updateSubTotal: function() {
+            var length = this.shoppingCart.length;
+            var t = 0;
+            for (var i=0; i<length; i++) {
+                t += this.shoppingCart[i].price;
+            }
+            this.subTotal = t;
+        },
+        buy: function(beer) {
+            this.shoppingCart.push(beer);
         }
-    }                
-});    
+    }
+});        
